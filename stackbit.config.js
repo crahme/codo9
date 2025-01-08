b52aa47cb1b84da34f3a129ddfb0ead9a7545582
crahme <66115159+crahme@users.noreply.github.com>
2025-01-08 23:07:36 +0400

    Update stackbit.config.js

--- stackbit.config.js
+++ stackbit.config.js
@@ -31,3 +31,15 @@ const config = {
 };
 
 export default config;
+
+// stackbit.config.ts new from content modeling for VE
+import { defineStackbitConfig } from "@stackbit/types";
+
+export default defineStackbitConfig({
+  // ...
+  modelExtensions: [
+    // Extend the "Page" and "Post" models by defining them as page models
+    { name: "Page", type: "page" },
+    { name: "Post", type: "page" }
+  ]
+});
