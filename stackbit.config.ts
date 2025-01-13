a5aa7f5856ce8d1abaa00b99e3f5fe32ce52fb29
crahme <66115159+crahme@users.noreply.github.com>
2025-01-13 17:00:09 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -5,7 +5,7 @@ export default defineStackbitConfig({
   // ...
   modelExtensions: [
     // Static URL paths derived from the model's "slug" field
-    { name: "Untitled", type: "Invoice", urlPath: "/{slug}" }
+    { name: "Invoice", type: "Invoice", urlPath: "/{slug}" }
   ], // <-- Missing comma added here
   siteMap: ({ documents, models }) => {
     // 1. Filter all page models which were defined in modelExtensions
