93fb7ac0ed7f6c242afd555ebe5f119a5e11f352
crahme <66115159+crahme@users.noreply.github.com>
2025-01-14 10:26:13 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -6,7 +6,7 @@ export default defineStackbitConfig({
   // ...
   modelExtensions: [
     // Static URL paths derived from the model's "slug" field
-    { name: "Untitled", type: "Invoice", urlPath: "/{slug}" }
+    { name: "Untitled", type: "Invoice", urlPath: "https://app.contentful.com/spaces/t3t3mhakehxg/visual_modeler/content_types" }
   ], // <-- Missing comma added here
   siteMap: ({ documents, models }) => {
     // 1. Filter all page models which were defined in modelExtensions
