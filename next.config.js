00c6f20b59c361e84421e59d8e6256962f863186
crahme <66115159+crahme@users.noreply.github.com>
2025-04-05 16:40:34 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   productionBrowserSourceMaps: true,
   env: {
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
   },
   webpack: (config, { isServer }) => {
