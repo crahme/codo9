9d577a309498d171744fc798c57985969e975f4f
crahme <66115159+crahme@users.noreply.github.com>
2025-04-09 16:18:11 +0400

    Update contentful.js

--- src/services/contentful.js
+++ src/services/contentful.js
@@ -2,7 +2,7 @@ import { createClient } from 'contentful';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
 });
 
 export async function fetchEntries() {
