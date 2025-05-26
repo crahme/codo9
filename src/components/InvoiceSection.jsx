e38089979315059468efb4fd57287cd047cf718b
crahme <66115159+crahme@users.noreply.github.com>
2025-05-26 10:12:49 +0300

    Update InvoiceSection.jsx

--- src/components/InvoiceSection.jsx
+++ src/components/InvoiceSection.jsx
@@ -1,6 +1,7 @@
 // src/components/InvoiceSection.js
 import React from 'react'
 import PropTypes from 'prop-types'
+import VisualEditorComponent from '../../VisualEditorComponent';
 
 export default function InvoiceSection({ heading, invoice, theme }) {
   return (
