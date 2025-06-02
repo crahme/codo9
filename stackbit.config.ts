69d074cc6b4d82efe2bb88afb721e413446ec2d7
crahme <66115159+crahme@users.noreply.github.com>
2025-06-02 11:56:29 +0300

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -31,7 +31,6 @@ export default defineStackbitConfig({
     { name: 'hero', type: 'data' },
     { name: 'stats', type: 'data' },
     { name: 'button', type: 'data' },
-    { name: 'invoice', type: 'data'},
     { name: 'statItem', type: 'data' },
     { name: 'invoiceSection', type: 'data' }, // Only if it exists
   ],
