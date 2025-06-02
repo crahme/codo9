81db5c2203023829afc8fa8953933af033a10e4b
crahme <66115159+crahme@users.noreply.github.com>
2025-06-02 12:35:41 +0300

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -26,12 +26,11 @@ export default defineStackbitConfig({
   modelExtensions: [
 
      { name: 'page', type: 'page', urlPath: '/{slug}' },
-    { type: 'page', urlPath: '/invoice/{slug}' },
+    { name:'invoice/, type: 'page', urlPath: '/invoice/{slug}' },
 
     { name: 'hero', type: 'data' },
     { name: 'stats', type: 'data' },
     { name: 'button', type: 'data' },
-    {name: 'invoice', type: 'data'},
     { name: 'statItem', type: 'data' },
     { name: 'invoiceSection', type: 'data' }, // Only if it exists
   ],
