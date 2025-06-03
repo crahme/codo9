7746fc44358a15839d92997fcc777d65624e2b13
crahme <66115159+crahme@users.noreply.github.com>
2025-06-03 11:03:54 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import { Stats } from '../components/Stats.jsx'; // Verify path
 import { getPageFromSlug } from '../utils/content.js'; // Verify path
 import { InvoiceSection } from '../components/InvoiceSection.jsx';
 import { Invoice } from '../components/Invoice.jsx';
-import { InvoiceLineItem } from '../components/ InvoiceLineItem.jsx'
+import { InvoiceLineItem } from '../components/ InvoiceLineItem.jsx';
 import { VisualEditorComponent } from '../components/VisualEditorComponent.jsx';
 // Map Contentful Content Type IDs to React components
 const componentMap = {
