a90d4358aaee9d50f33283d3e0b928a4cce4d7c0
crahme <crahme82@gmail.com>
2025-08-08 11:28:19 +0300

    Update some files

--- netlify/functions/generateinvoice.js
+++ netlify/functions/generateinvoice.js
@@ -5,8 +5,8 @@ const CloudOceanAPI = require('../../services/cloudoceanapi');
 exports.handler = async (event) => {
   const moduleUuid = "c667ff46-9730-425e-ad48-1e950691b3f9";
   const measuringPointUuid = "71ef9476-3855-4a3f-8fc5-333cfbf9e898";
-  const start = "2024-10-16";
-  const end = "2024-11-25";
+  const start = new Date("2024-10-16");
+  const end = new Date("2024-11-25");
   const rate = 0.12;
 
   const cloudOcean = new CloudOceanAPI(process.env.API_Key);
