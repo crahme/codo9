58c6bae3c77eea911da7ca0047f4d7ecf2e8666d
crahme <crahme82@gmail.com>
2025-08-19 10:18:57 +0300

    Main

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import { Sequelize, DataTypes, Model } from 'sequelize';
-const path = require('path');
-const fs = require('fs');
+import path from 'path';
+import fs from  'fs';
 const logger = require('pino')({ level: 'info' });
 
-// Express app and DB setup
+// Express app and DB setup     
 const app = express();
 const DATABASE_URL = process.env.NETLIFY_DATABASE_URL || 'sqlite:./invoices.db';
 
