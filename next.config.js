be98fe91c24457b26e6ea7f6cb9676bdc6a5a59b
crahme <crahme82@gmail.com>
2025-08-21 17:26:26 +0300

    Revert next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { dev, isServer }) => {
+    
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+      },
+    ],
+  },
+  productionBrowserSourceMaps: true,
+  env: {
+    CONTENTFUL_MANAGEMENT_TOKEN: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+    CONTENTFUL_DELIVERY_TOKEN: process.env.CONTENTFUL_DELIVERY_TOKEN,
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+  },
+  allowedDevOrigins: [
+    'https://devserver-preview--codo9.netlify.app',
+    'http://localhost:3000',
+    // Add more dev/preview URLs if needed
+  ],
+    webpack: (config, { dev, isServer }) => {
     if (dev && !isServer) {
       // Only suppress source map warnings, don't change devtool
       config.ignoreWarnings = [
@@ -13,6 +34,14 @@ const nextConfig = {
     }
     return config;
   },
-}
 
-module.exports = nextConfig;
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  }
+  // Uncomment and adjust if you use custom caching:
+  // cacheHandler: require('./cache-handler.js'),
+  // cacheMaxMemorySize: 0,
+};
+
+export default nextConfig;
