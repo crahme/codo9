5e421c710e1c6f856697dbd2accaafc530bc2b2a
crahme <crahme82@gmail.com>
2025-09-02 09:42:30 +0300

    Change main

--- main.mjs
+++ main.mjs
@@ -8,7 +8,7 @@ import pino from 'pino';
 import app from './app.mjs'; // Import the Express app from app.mjs
 const logger = pino({ level: 'info' });
 // Express app and DB setup     
-const app = express();
+
 const DATABASE_URL = process.env.NETLIFY_DATABASE_URL || 'sqlite:./invoices.db';
 
 const PORT = 5000;
