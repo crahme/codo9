f3ecff1a9b708e4e02a16b596373f3fd9f606f76
crahme <crahme82@gmail.com>
2025-09-15 15:52:46 +0300

    Change

--- src/scripts/rve-to-contentful.js
+++ src/scripts/rve-to-contentful.js
@@ -2,7 +2,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { createClient } from 'contentful-management';
+import pkg from 'contentful-management';
+const { createClient } = pkg;
 import {CloudOceanService} from '../services/CloudOceanService.js';
 import PDFDocument from 'pdfkit';
 import fs from 'fs';
