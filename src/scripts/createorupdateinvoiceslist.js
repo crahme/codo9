efec12f48ce4f018d359329eb44b581980cb117e
crahme <crahme82@gmail.com>
2025-09-23 12:02:27 +0300

    Fix

--- src/scripts/createorupdateinvoiceslist.js
+++ src/scripts/createorupdateinvoiceslist.js
@@ -93,7 +93,7 @@ async function main() {
   // Save and publish
   await entry.update();
   await entry.publish();
-  console.log("✅ InvoicesList entry updated successfully");
+  console.log("✅ InvoicesList entry updated successfully", InvoicesList);
 }
 
 main().catch(err => {
