d65ab779806b2471ae1564d3f95c3fdc8e259327
crahme <crahme82@gmail.com>
2025-09-27 10:47:31 +0300

    New

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-// next.config.js
 import bundleAnalyzer from '@next/bundle-analyzer';
 
 const withBundleAnalyzer = bundleAnalyzer({
@@ -8,19 +7,6 @@ const withBundleAnalyzer = bundleAnalyzer({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // 👇 This tells the Visual Editor to ignore API routes
-  experimental: {
-    stackbit: {
-      guard: (context) => {
-        // If the request is for our invoices API route -> block it
-        if (context?.url?.startsWith('/api/invoices')) {
-          return false
-        }
-        return true
-      },
-    },
-  },
 };
 
 export default withBundleAnalyzer(nextConfig);
