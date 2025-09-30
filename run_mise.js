7bd4fa0bb7ecacb813aadd80b48cd7c52f3648ff
crahme <crahme82@gmail.com>
2025-09-30 13:05:45 +0300

    Fix mise runner again

--- run_mise.js
+++ run_mise.js
@@ -4,7 +4,7 @@ const { Mise } = pkg;
 async function main() {
   // Create a new Mise instance
   
-
+  const mise = new Mise();
   try {
     // Add the setting
     await mise.settings.add('idiomatic_version_file_enable_tools', 'node');
