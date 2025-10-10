997f09f2504ca5ac7cd8f9996bd65f6b34b09295
crahme <crahme82@gmail.com>
2025-10-10 14:40:05 +0300

    New

--- src/scripts/rve-to-contentful.js
+++ src/scripts/rve-to-contentful.js
@@ -73,7 +73,7 @@ function generateInvoicePDF(invoiceData) {
   doc.moveDown();
 
   // --- Station Info ---
-  doc.fontSize(20).text("Invoice Details", { align: "left" });
+  doc.fontSize(20).text("Invoice Details", { align: "left" }, indent-20);
   doc.fontSize(12).text("Invoice: " + invoiceData.invoiceNumber, indent);
   doc.moveDown(1);
   doc.text("Date: " + invoiceData.invoiceDate, indent);
